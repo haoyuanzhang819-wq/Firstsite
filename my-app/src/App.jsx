2bc50923f8fcc28736437796310a6abe886c947f
haoyuanzhang819-wq <haoyuanzhang819@gmail.com>
2026-06-01 15:03:08 +0200

    feat: first React app, Counter component

---
+++ my-app/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Counter from './Counter'
+
+function App() {
+  return (
+    <div>
+      <h1>Hi, I'm Hao</h1>
+      <p>my summer progress</p>
+      <Counter />
+      <Counter />
+    </div>
+  )
+}
+
+export default App
